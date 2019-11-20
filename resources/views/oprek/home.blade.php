194da5a4ebd34a883f36383b0de7851859e4e7f4
Ganendra Afrasya <gsculerlor@hotmail.com>
2019-11-21 00:02:23 +0700

    Fix badge not showing on pilihan 1

--- resources/views/oprek/home.blade.php
+++ resources/views/oprek/home.blade.php
@@ -71,7 +71,7 @@
                 <tbody>
                     @foreach ($rows['rows1'] as $row)
                     <tr>
-                        <td>{{ $row->nama }}</td>
+                        <td>{{ $row->nama }} <span class="badge badge-secondary">{{ $rows['dept'][$row->pilihan_satu] }}</span> <span class="badge badge-secondary">{{ $rows['dept'][$row->pilihan_dua] }}</span></td>
                         <td>{{ $row->nrp }}</td>
                         <td>{{ $row->alasan_pilihan_satu }}</td>
                         <td>
